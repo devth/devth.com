c67f6fb0c28523454878fd47f17a33c95aac677f
Trevor Hartman <trevorhartman@gmail.com>
2023-09-05 17:26:51 -0600

    Setup Logo and mess with Links

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,28 @@
-import { Container } from "@mui/material";
+import { Container, Stack } from "@mui/material";
+import { Logo } from "./Logo";
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import MuiLink, { LinkProps as MuiLinkProps } from "@mui/material/Link";
+
+const maxWidth = "lg";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Container maxWidth="lg">{children}</Container>;
+  return (
+    <>
+      <header>
+        <nav>
+          <Container maxWidth={maxWidth}>
+            <Stack direction="row" spacing={2} alignItems="center">
+              <NextLink href="/">
+                <Logo />
+              </NextLink>
+              <MuiLink component={NextLink} href="/about">
+                About
+              </MuiLink>
+            </Stack>
+          </Container>
+        </nav>
+      </header>
+      <Container maxWidth={maxWidth}>{children}</Container>
+    </>
+  );
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,3 @@
+export function Nav(): React.ReactElement {
+  return <div>Nav</div>;
+}
