14b9a63c38d5cb6c2bd4044d26a8dd58d22d12e0
Trevor Hartman <trevorhartman@gmail.com>
2023-09-05 18:38:44 -0600

    Disable redirect

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,11 @@ const nextConfig = {
   /* config options here */
   async redirects() {
     return [
-      {
-        source: "/:year/:path*",
-        destination: "/:path*",
-        permanent: true,
-      },
+      // {
+      //   source: "/:year/:path*",
+      //   destination: "/:path*",
+      //   permanent: true,
+      // },
     ];
   },
 };
