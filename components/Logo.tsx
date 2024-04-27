fb2ddc036090151df48d9da448f5100ade46c97a
Trevor Hartman <trevorhartman@gmail.com>
2024-04-26 18:11:01 -0600

    Try scaling down for mobile nav (#28)

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,13 +2,17 @@ import MuiLink from "@mui/material/Link";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import { useIsDarkMode } from "../hooks/useIsDarkMode";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 export const Logo = () => {
   const router = useRouter();
   const isDarkMode = useIsDarkMode();
   const logoColor = isDarkMode ? "#666" : "#ccc";
   const isHome = router.pathname === "/";
-  const scale = isHome ? 1 : 0.5;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const scale = isHome && !isMobile ? 1 : 0.5;
+  const scaleDuration = isMobile ? 0 : 0.5;
   const fontSize = 60 * scale;
 
   return (
@@ -17,7 +21,7 @@ export const Logo = () => {
         sx={{
           fontSize: `${fontSize}px`,
           fontWeight: "bold",
-          transition: "all .3s ease-out 0s, font-size .4s ease-out 0.5s",
+          transition: `all .3s ease-out 0s, font-size ${scaleDuration}s ease-out 0.5s`,
           color: logoColor,
           "&:hover": {
             color: isDarkMode ? "#eee" : "black",
